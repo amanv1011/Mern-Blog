aed8d09914bac6bbf7f91d1fac54c0bd08b3d03d
Aman <aman@Amn.local>
2024-03-19 12:57:21 +0530

    name

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,10 +43,10 @@ export default function SignUp() {
         {/* left */}
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Sahand's</span>
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white mr-1">HaHaHaven</span>
             Blog
           </Link>
-          <p className="text-sm mt-5">This is a demo project. You can sign up with your email and password or with Google.</p>
+          <p className="text-sm mt-5">You can sign up with your email and password or with Google.</p>
         </div>
         {/* right */}
 
--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -116,10 +116,10 @@ export default function UpdatePost() {
             value={formData.title}
           />
           <Select onChange={(e) => setFormData({ ...formData, category: e.target.value })} value={formData.category}>
-            <option value="uncategorized">Select a category</option>
-            <option value="javascript">Comedy</option>
-            <option value="reactjs">Dance</option>
-            <option value="nextjs">Music</option>
+            <option value="">Select a category</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Dance">Dance</option>
+            <option value="Music">Music</option>
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
