a975a7632df3c9573fb91084c4035af5852b8f28
aman verma <amanv1011@gmail.com>
2024-04-27 14:06:01 +0530

    facebookAuthenticate

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -5,23 +5,27 @@ import { app } from "../firebase";
 import { useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
+import {LoginSocialFacebook } from "reactjs-social-login"
+import { FaFacebook } from "react-icons/fa";
+// import {FacebookLoginButton } from "react-social-login-button"
+
 
 export default function OAuth() {
   const auth = getAuth(app);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleGoogleClick = async () => {
+  const handleGoogleClick = async (resp) => {
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
     try {
-      const resultsFromGoogle = await signInWithPopup(auth, provider);
+      // const resultsFromGoogle = await signInWithPopup(auth, provider);
       const res = await fetch("/api/auth/google", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          name: resultsFromGoogle.user.displayName,
-          email: resultsFromGoogle.user.email,
-          googlePhotoUrl: resultsFromGoogle.user.photoURL,
+          name: resp.data.name,
+          email: resp.data.email,
+          googlePhotoUrl: resp.data.picture.data.url,
         }),
       });
       const data = await res.json();
@@ -34,9 +38,67 @@ export default function OAuth() {
     }
   };
   return (
-    <Button type="button" gradientDuoTone="pinkToOrange" outline onClick={handleGoogleClick}>
+    <>
+      
+<LoginSocialFacebook 
+appId = "433322819300792"
+onResolve = {(res)=>handleGoogleClick(res)}
+// onResolve = {handleGoogleClick}
+onReject = {(err)=>{console.log("Getting error",err)}}
+>
+<FaFacebook/>
+</LoginSocialFacebook>
+
+    {/* <Button type="button" gradientDuoTone="pinkToOrange" outline onClick={handleGoogleClick}>
       <AiFillGoogleCircle className="w-6 h-6 mr-2" />
       Continue with Google
-    </Button>
+    </Button> */}
+    </>
   );
 }
+
+
+
+
+// import { Button } from "flowbite-react";
+// import { AiFillGoogleCircle } from "react-icons/ai";
+// import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+// import { app } from "../firebase";
+// import { useDispatch } from "react-redux";
+// import { signInSuccess } from "../redux/user/userSlice";
+// import { useNavigate } from "react-router-dom";
+
+// export default function OAuth() {
+//   const auth = getAuth(app);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const handleGoogleClick = async () => {
+//     const provider = new GoogleAuthProvider();
+//     provider.setCustomParameters({ prompt: "select_account" });
+//     try {
+//       const resultsFromGoogle = await signInWithPopup(auth, provider);
+//       const res = await fetch("/api/auth/google", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/json" },
+//         body: JSON.stringify({
+//           name: resultsFromGoogle.user.displayName,
+//           email: resultsFromGoogle.user.email,
+//           googlePhotoUrl: resultsFromGoogle.user.photoURL,
+//         }),
+//       });
+//       const data = await res.json();
+//       if (res.ok) {
+//         dispatch(signInSuccess(data));
+//         navigate("/");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//   return (
+//     <Button type="button" gradientDuoTone="pinkToOrange" outline onClick={handleGoogleClick}>
+//       <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+//       Continue with Google
+//     </Button>
+//   );
+// }
